a427ec95f165ac2f8d3aaac4fbd7ea1ae4ba80e9
Legatus88 <y1wkn8@gmail.com>
2018-09-28 07:24:20 +0400

    added MyApp class

--- app.rb
+++ app.rb
@@ -1,4 +1,5 @@
 require 'sinatra'
+require 'sinatra/base'
 require 'sinatra/activerecord'
 require 'active_support'
 
@@ -9,31 +10,34 @@ use Rack::Auth::Basic, "Restricted Area" do |username, password|
   username == 'sportsman' and password == 'sportsmans_password'
 end
 
-get '/' do 
-  @title = 'Новая запись'
-  haml :index
-end
 
-post '/record/new' do 
-  @record = Record.create(params)
-  redirect to '/list' if @record.save
-end
+class MyApp < Sinatra::Base
+  get '/' do 
+    @title = 'Новая запись'
+    haml :index
+  end
 
-get '/list' do
-  @records = Record.all.order(:record_date)
-  haml :list
-end
+  post '/record/new' do 
+    @record = Record.create(params)
+    redirect to '/list' if @record.save
+  end
 
-delete '/records/:id' do
-  @record = Record.find(params[:id])
-  @record.delete
-  redirect to '/list'
-end
+  get '/list' do
+    @records = Record.all.order(:record_date)
+    haml :list
+  end
 
-get '/chart' do 
-  @dates = Record.all.map(&:record_date)
-  @weights = Record.all.map(&:weight)
-  haml :chart
+  delete '/records/:id' do
+    @record = Record.find(params[:id])
+    @record.delete
+    redirect to '/list'
+  end
+
+  get '/chart' do 
+    @dates = Record.all.map(&:record_date)
+    @weights = Record.all.map(&:weight)
+    haml :chart
+  end
 end
 
 require './models'
